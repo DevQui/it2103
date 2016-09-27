5907e83f404f4450100be03aa8f998bb46ec3fa2
Kjm517 <kajama517@gmail.com>
2016-09-27 14:15:23 +0800

    added a glyphicon

--- home.php
+++ home.php
@@ -3,6 +3,8 @@
 		<link rel='stylesheet' href="css/bootstrap.css">
 	</head>
 	<body>
-		<button class='btn btn-danger'>Red Button daw ni</button>
+		<button class='btn btn-danger'>
+			<span class='glyphicon glyphicon-user'></span>
+		</button>
 	</body>
 </html>
